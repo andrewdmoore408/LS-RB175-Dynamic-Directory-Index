8d53791deca79312cc69feb7d80ba7f6951864b7
Andrew Moore <andrewdmoore84@gmail.com>
2021-08-02 00:01:03 -0400

    Add sorting for files and option to select descending sort

--- dynamic_dir.rb
+++ dynamic_dir.rb
@@ -2,11 +2,19 @@ require 'tilt/erubis'
 require 'sinatra'
 require 'sinatra/reloader'
 
-get "/" do
+get "/:sort_order" do
   @title = "Dynamic Directory"
   @wd = "public"
 
+  sort_ascending = (params['sort_order'] == 'reverse' ? false : true)
+
   @files_only = Dir.children(@wd).reject { |item| File.directory?("#{@wd}/#{item}") }
+
+  @files_sorted = if sort_ascending
+                    @files_only.sort
+                  else
+                    @files_only.sort.reverse
+                  end
 
   erb :index
 end
